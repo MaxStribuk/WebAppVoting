bffa20106978ee12965c607a3753eebf3b679ead
Artur <sharam.artur@gmail.com>
2022-12-09 18:05:27 +0300

    changed interface MusicianDAO and realisation of class MusicianMemoryDAO

--- src/main/java/dao/MusicianMemoryDAO.java
+++ src/main/java/dao/MusicianMemoryDAO.java
@@ -1,16 +1,20 @@
 package dao;
 
 import dao.api.MusicianDAO;
+import dto.GenreDTO;
+import dto.MusicianDTO;
 
 import java.util.List;
 
 public class MusicianMemoryDAO implements MusicianDAO {
 
     private static volatile MusicianMemoryDAO instance = null;
-    private final List<String> musicians = List.of("Taylor Swift",
-            "Prince",
-            "Elvis Presley",
-            "Eminem");
+    private final List<MusicianDTO> musicians = List.of(
+            new MusicianDTO(1,"Taylor Swift"),
+            new MusicianDTO(2,"Prince"),
+            new MusicianDTO(3,"Elvis Presley"),
+            new MusicianDTO(4,"Eminem")
+                    );
 
     private MusicianMemoryDAO() {
     }
@@ -26,12 +30,18 @@ public class MusicianMemoryDAO implements MusicianDAO {
         return instance;
     }
     @Override
-    public List<String> getAllMusicians() {
+    public List<MusicianDTO> getAllMusicians() {
         return musicians;
     }
 
     @Override
     public boolean exists(String musician) {
-        return musicians.contains(musician);
+        List<MusicianDTO> musicianDTOS = getAllMusicians();
+        for (MusicianDTO musicianDTO: musicianDTOS) {
+            if(musician.equals(musicianDTO.getMusician())){
+                return true;
+            }
+        }
+        return false;
     }
 }
