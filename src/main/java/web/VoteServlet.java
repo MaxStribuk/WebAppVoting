b139ce8dc648491f3775fc8059a61ff6a31526c6
Dmitry Kulinenko <kulinenko34@gmail.com>
2022-12-11 11:52:07 +0300

    changed VoteServlet

--- src/main/java/web/VoteServlet.java
+++ src/main/java/web/VoteServlet.java
@@ -1,5 +1,6 @@
 package web;
 
+import dto.SavedVoteDTO;
 import dto.VoteDTO;
 import service.api.IVoteService;
 import service.factories.VoteServiceSingleton;
@@ -10,6 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "VoteServlet", urlPatterns = "/vote")
 public class VoteServlet extends HttpServlet {
@@ -20,22 +24,39 @@ public class VoteServlet extends HttpServlet {
 
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
+
+        String artistId = req.getParameter("artist");
+        if (artistId == null || artistId.isBlank()) {
+            throw new IllegalArgumentException("User failed to provide artist id");
+        }
+
+        String[] genreIds = req.getParameterValues("genre");
+        if (genreIds == null) {
+            throw new IllegalArgumentException("User failed to provide genre id");
+        }
+        for (String genreId : genreIds) {
+            if (genreId.isBlank()) {
+                throw new IllegalArgumentException("User failed to provide genre id");
+            }
+        }
+
+        String message = req.getParameter("message");
+        if (message == null || message.isBlank()) {
+            throw new IllegalArgumentException("User failed to provide artist id");
+        }
+
         IVoteService service = VoteServiceSingleton.getInstance();
 
-        String musician = req.getParameter("artist");
-        String[] genres = req.getParameterValues("genre");
-        String username = req.getParameter("username");
-        String message = req.getParameter("message");
+        int musicianId = Integer.parseInt(artistId);
+        List<Integer> genres = Arrays.stream(genreIds).map(Integer::parseInt).collect(Collectors.toList());
 
-        UserMessage userMessage = new UserMessage(username, message);
-        VoteDTO vote = new VoteDTO(musician, genres, userMessage);
+        VoteDTO vote = new VoteDTO(musicianId, genres, message);
 
         service.validate(vote);
-        service.save(vote);
+        service.save(new SavedVoteDTO(vote));
 
         PrintWriter writer = resp.getWriter();
-        writer.append("Thank you for your response, ").append(username);
+        writer.append("Thank you for your response, ");
     }
-
 
 }
