57b55786ab4d29fadc70abcfd446238f6fbfc129
MaxStribuk <maximus_747@mail.ru>
2022-12-12 14:19:25 +0300

    update ResultServlet, added processing StatisticsDTO

--- src/main/java/web/ResultsServlet.java
+++ src/main/java/web/ResultsServlet.java
@@ -1,5 +1,6 @@
 package web;
 
+import dto.StatisticsDTO;
 import service.api.IStatisticsService;
 import service.factories.StatisticsServiceSingleton;
 
@@ -18,15 +19,43 @@ public class ResultsServlet extends HttpServlet {
     public ResultsServlet() {
         this.service = StatisticsServiceSingleton.getInstance();
     }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
 
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
-        String results = service.getStringValue();
+
+        StatisticsDTO statistics = service.getStatistics();
         PrintWriter writer = resp.getWriter();
 
-        writer.write(results);
+        writeBestArtists(writer, statistics);
+        writeBestGenres(writer, statistics);
+        writeAbouts(writer, statistics);
+    }
+
+    private void writeBestArtists(PrintWriter writer, StatisticsDTO statistics) {
+        statistics.getBestArtists()
+                .forEach((key, value) -> writer.append(key.getArtist())
+                        .append(" - ")
+                        .append(value.toString())
+                        .append(" votes<br>"));
+    }
+
+    private void writeBestGenres(PrintWriter writer, StatisticsDTO statistics) {
+        statistics.getBestGenres()
+                .forEach((key, value) -> writer.append(key.getGenre())
+                        .append(" - ")
+                        .append(value.toString())
+                        .append(" votes<br>"));
+    }
+
+    private void writeAbouts(PrintWriter writer, StatisticsDTO statistics) {
+        statistics.getAbouts()
+                .forEach((key, value) -> writer.append(key.toString())
+                        .append(" - ")
+                        .append(value)
+                        .append("<br>"));
     }
 }
