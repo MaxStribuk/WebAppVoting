e1b82a2ac632d47f37d0b3a480762cdb87013d14
Dmitry Kulinenko <kulinenko34@gmail.com>
2023-01-11 23:38:01 +0300

    added new methods to the interface and class

--- src/main/java/dao/api/IGenreDAO.java
+++ src/main/java/dao/api/IGenreDAO.java
@@ -11,4 +11,10 @@ public interface IGenreDAO {
     boolean exists(int id);
 
     GenreDTO get(int id);
+
+    void add(String artist);
+
+    void update(int id, String artist);
+
+    void delete(int id);
 }
