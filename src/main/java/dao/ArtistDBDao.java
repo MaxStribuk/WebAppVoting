a67b6b5a3d29a483bdd80dd7ac298955e6402f9c
Artur <sharam.artur@gmail.com>
2023-01-11 16:13:23 +0300

    add static

--- src/main/java/dao/ArtistDBDao.java
+++ src/main/java/dao/ArtistDBDao.java
@@ -16,11 +16,11 @@ public class ArtistDBDao implements IArtistDAO {
     private static final String GET_ALL = "SELECT id, name FROM app.artist";
     private static final String GET = "SELECT id, name FROM app.artist " +
             "WHERE id = ?";
-    private final String ADD = "INSERT INTO app.artist (name) VALUES (?);";
-    private final String UPDATE = "UPDATE app.artist SET name=? WHERE id=?;";
-    private final String COUNT_VOTES = "SELECT COUNT(id) AS count FROM app.votes " +
+    private static final String ADD = "INSERT INTO app.artist (name) VALUES (?);";
+    private static final String UPDATE = "UPDATE app.artist SET name=? WHERE id=?;";
+    private static final String COUNT_VOTES = "SELECT COUNT(id) AS count FROM app.votes " +
             "GROUP BY artist_id HAVING artist_id=?;";
-    private final String DELETE = "DELETE FROM app.artist WHERE id=?;";
+    private static final String DELETE = "DELETE FROM app.artist WHERE id=?;";
 
     @Override
     public List<ArtistDTO> getAll() {
