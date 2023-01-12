01cc80df2e73254567d903f163fe857f39b3f97c
Alexander <gandalfdude@gmail.com>
2023-01-12 20:04:30 +0300

    added support for patch http method

---
+++ src/main/java/web/ExtendedHttpServlet.java
@@ -0,0 +1,23 @@
+package web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class ExtendedHttpServlet extends HttpServlet {
+
+    public void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase("PATCH")){
+            doPatch(request, response);
+        } else {
+            super.service(request, response);
+        }
+    }
+
+    public abstract void doPatch(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException;
+
+}
