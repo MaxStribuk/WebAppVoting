24ebd2c9d92afe3dd33a65f82e56f55bf3308198
MaxStribuk <maximus_747@mail.ru>
2023-01-16 15:06:08 +0300

    update GenreService

--- src/main/java/service/GenreService.java
+++ src/main/java/service/GenreService.java
@@ -20,12 +20,43 @@ public class GenreService implements IGenreService {
     }
 
     @Override
-    public GenreDTO getGenreById(int id) {
+    public GenreDTO get(int id) {
         return dataSource.get(id);
     }
 
     @Override
     public boolean exists(int id) {
         return dataSource.exists(id);
+    }
+
+    @Override
+    public void add(String genre) {
+        boolean isRecurringGenre = getAll().stream()
+                .map(GenreDTO::getGenre)
+                .anyMatch(nameGenre -> nameGenre.equalsIgnoreCase(genre));
+        if (isRecurringGenre) {
+            throw new IllegalArgumentException("This genre has already " +
+                    "been added");
+        } else {
+            dataSource.add(genre);
+        }
+    }
+
+    @Override
+    public void update(int id, String genre) {
+        if (dataSource.exists(id)) {
+            dataSource.update(id, genre);
+        } else {
+            throw new IllegalArgumentException("No genre updated for id " + id);
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        if (dataSource.exists(id)) {
+            dataSource.delete(id);
+        } else {
+            throw new IllegalArgumentException("No genre deleted for id " + id);
+        }
     }
 }
