3f883b303e59ccbe7f0f22f3a54114a143023c8f
MaxStribuk <maximus_747@mail.ru>
2023-01-30 20:00:44 +0300

    update EmailResendLoaderListener

--- src/main/java/web/listeners/EmailResendLoaderListener.java
+++ src/main/java/web/listeners/EmailResendLoaderListener.java
@@ -3,11 +3,11 @@ package web.listeners;
 import service.api.ISendingService;
 import service.factories.SenderServiceSingleton;
 
-import javax.mail.MessagingException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.util.concurrent.TimeUnit;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class EmailResendLoaderListener implements ServletContextListener {
 
     private final ISendingService sendingService;
@@ -18,23 +18,11 @@ public class EmailResendLoaderListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        while (true) {
-            sendingService.getExecutorService().scheduleWithFixedDelay(() -> {
-                try {
-                    sendingService.resend();
-                } catch (MessagingException e) {
-                    try {
-                        TimeUnit.HOURS.sleep(1L);
-                    } catch (InterruptedException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }
-            }, 60L, 5L, TimeUnit.SECONDS);
-        }
+        sendingService.initializeSendingService();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        sendingService.getExecutorService().shutdown();
+        sendingService.stopSendingService();
     }
 }
