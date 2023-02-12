a75c560b9de14f186889941021a74b03aabe1103
MaxStribuk <maximus_747@mail.ru>
2023-02-12 15:30:45 +0300

    update vote controller for spring-mvc

---
+++ src/main/java/dto/request/VoteDTORequest.java
@@ -0,0 +1,59 @@
+package dto.request;
+
+import java.util.List;
+import java.util.Objects;
+
+public class VoteDTORequest {
+
+    private final Long artistId;
+    private final List<Long> genreIds;
+    private final String about;
+    private final String email;
+
+    public VoteDTORequest(Long artistId, List<Long> genreIds,
+                          String about, String email) {
+        this.artistId = artistId;
+        this.genreIds = genreIds;
+        this.about = about;
+        this.email = email;
+    }
+
+    public Long getArtistId() {
+        return artistId;
+    }
+
+    public List<Long> getGenreIds() {
+        return genreIds;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VoteDTORequest that = (VoteDTORequest) o;
+        return artistId.equals(that.artistId) && genreIds.equals(that.genreIds) && about.equals(that.about) && email.equals(that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(artistId, genreIds, about, email);
+    }
+
+    @Override
+    public String toString() {
+        return "VoteDTORequest{" +
+                "artistId=" + artistId +
+                ", genreIds=" + genreIds +
+                ", about='" + about + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/web/controllers/VoteController.java
@@ -0,0 +1,24 @@
+package web.controllers;
+
+import dto.request.VoteDTORequest;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.api.IVoteService;
+
+@RestController
+@RequestMapping("/vote")
+public class VoteController {
+
+    private final IVoteService voteService;
+
+    public VoteController(IVoteService voteService) {
+        this.voteService = voteService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void save(VoteDTORequest vote) {
+        voteService.save(vote);
+    }
+}
